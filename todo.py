0daba8403ab2db5110561626b5ea4bb1bb81c4b8
Bhavesh Kumar Parmar <95556964+Bhavesh8890@users.noreply.github.com>
2025-05-26 15:24:23 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -8,6 +8,7 @@ def load_tasks():
         with open(TODO_FILE, "r") as file:
             return json.load(file)
     return []
+
 def save_tasks(tasks):
     with open(TODO_FILE, "w") as file:
         json.dump(tasks, file, indent=2)
